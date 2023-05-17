c61771b8c9a1186a06e149b4be948c8d49f24530
tokushun109 <tokushun109@gmail.com>
2023-05-18 04:32:25 +0900

    ディレクトリ構成の初期化

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
-import '@/styles/globals.scss'
+import '@/pages/styles/globals.scss'
 import type { AppProps } from 'next/app'
+
+import 'modern-css-reset/dist/reset.min.css'
 
 export default function App({ Component, pageProps }: AppProps) {
     return <Component {...pageProps} />
